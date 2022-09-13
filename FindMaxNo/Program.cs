2285bcecbc973362259834da2e56f3de24d1722f
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-09-13 16:47:59 +0530

    [Prajakta] Add: Display Welcome Message

---
+++ FindMaxNo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FindMaxNo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Generic Maximum Number Problem");
+        }
+    }
+}
